929295e259ab1309ca20eca26542a6691995befb
Israel Ferreira dos Santos <48022462+RaelZ@users.noreply.github.com>
2024-04-26 16:29:03 -0300

    update: vite to next

---
+++ front/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="">
+      <div className="bg-gradient-to-b from-cyan-500 to-blue-500 h-[100dvh]">
+        sfinslkdfjn
+      </div>
+    </main>
+  )
+}
